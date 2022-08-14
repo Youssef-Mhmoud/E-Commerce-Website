70198aa5db4a9d2e9fef750b1bf31a8c73eca3c8
Youssef_Mahmoud <100860879+Youssef-Mhmoud@users.noreply.github.com>
2022-08-14 07:58:53 +0200

    Edit Slider

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -8,64 +8,67 @@ import { faAngleRight, faAngleLeft } from "@fortawesome/free-solid-svg-icons";
 const Home = () => {
   const [data, setData] = useState(SliderData);
   const [index, setIndex] = useState(0);
-  
+
   useEffect(() => {
-    const lastIndex = data.length -1 ;
-    
+    const lastIndex = data.length - 1;
+
     if (index < 0) {
-      setIndex(lastIndex)
+      setIndex(lastIndex);
     } else if (index > lastIndex) {
-      setIndex(0)
+      setIndex(0);
     }
-  }, [index, data])
+  }, [index, data]);
 
   useEffect(() => {
     const slider = setTimeout(() => {
-      setIndex(autoIndex => autoIndex + 1)
-    }, 5000)
+      setIndex((autoIndex) => autoIndex + 1);
+    }, 5000);
     return () => {
-      clearInterval(slider)
-    }
-  }, [index])
+      clearInterval(slider);
+    };
+  }, [index]);
 
   const handleRight = () => {
     setIndex((prevIndex) => prevIndex + 1);
-  }
+  };
   const handleLeft = () => {
     setIndex((prevIndex) => prevIndex - 1);
-  }
+  };
 
   return (
     <>
       <div className="container home-pg">
-        <div className="slider">
-          {data.map((image, imgIndex) => {
-            const { img, id } = image;
-            let postion = "right";
-            if (imgIndex === index) {
-              postion = "active";
-            } else if (
-              imgIndex === index - 1 ||
-              (index === 0 && imgIndex === data.length - 1)
-            ) {
-              postion = "left";
-            }
-            return (
-              <div className={`img ${postion}`} key={id}>
-                <img src={img} />
-              </div>
-            );
-          })}
-          <FontAwesomeIcon
-            className="angelLeft"
-            onClick={handleLeft}
-            icon={faAngleLeft}
-          />
-          <FontAwesomeIcon
-            className="angelRight"
-            onClick={handleRight}
-            icon={faAngleRight}
-          />
+        <div className="landing">
+          <div className="h">Hello world</div>
+          <div className="slider">
+            {data.map((image, imgIndex) => {
+              const { img, id } = image;
+              let postion = "right";
+              if (imgIndex === index) {
+                postion = "active";
+              } else if (
+                imgIndex === index - 1 ||
+                (index === 0 && imgIndex === data.length - 1)
+              ) {
+                postion = "left";
+              }
+              return (
+                <div className={`img ${postion}`} key={id}>
+                  <img src={img} />
+                </div>
+              );
+            })}
+            <FontAwesomeIcon
+              className="angelLeft"
+              onClick={handleLeft}
+              icon={faAngleLeft}
+            />
+            <FontAwesomeIcon
+              className="angelRight"
+              onClick={handleRight}
+              icon={faAngleRight}
+            />
+          </div>
         </div>
       </div>
     </>
