601bdf40e35c7168b04239bce4340c2633bfd8fd
Youssef_Mahmoud <100860879+Youssef-Mhmoud@users.noreply.github.com>
2022-08-15 05:42:17 +0200

    Adding Side Bar

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -4,30 +4,35 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./index.scss";
 import sale from "../../assests/imgs/sale.jpg";
 import { faAngleRight, faAngleLeft } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
 
 const Home = () => {
+  // Start Slider UseState
   const [data, setData] = useState(SliderData);
   const [index, setIndex] = useState(0);
+  // End Slider UseState
 
-  useEffect(() => {
-    const lastIndex = data.length - 1;
+  // Slider
+  {
+    useEffect(() => {
+      const lastIndex = data.length - 1;
 
-    if (index < 0) {
-      setIndex(lastIndex);
-    } else if (index > lastIndex) {
-      setIndex(0);
-    }
-  }, [index, data]);
+      if (index < 0) {
+        setIndex(lastIndex);
+      } else if (index > lastIndex) {
+        setIndex(0);
+      }
+    }, [index, data]);
 
-  useEffect(() => {
-    const slider = setTimeout(() => {
-      setIndex((autoIndex) => autoIndex + 1);
-    }, 5000);
-    return () => {
-      clearInterval(slider);
-    };
-  }, [index]);
-
+    useEffect(() => {
+      const slider = setTimeout(() => {
+        setIndex((autoIndex) => autoIndex + 1);
+      }, 5000);
+      return () => {
+        clearInterval(slider);
+      };
+    }, [index]);
+  }
   const handleRight = () => {
     setIndex((prevIndex) => prevIndex + 1);
   };
@@ -39,7 +44,32 @@ const Home = () => {
     <>
       <div className="container home-pg">
         <div className="landing">
-          <div className="h">Hello world</div>
+          <div className="main-side">
+            <aside className="side-bar side1">
+              <ul className="list-side">
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/">About</Link>
+                </li>
+                <li>
+                  <Link to="/">Cart</Link>
+                </li>
+              </ul>
+            </aside>
+            <aside className="side-bar side2">
+              <h2>About</h2>
+              <p className="info-about">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Vestibulum molestie vulputate mollis. Ut maximus tortor eros, ut
+                interdum nulla semper vel. Nulla facilisi. Pellentesque habitant
+              </p>
+                <Link to='/' className="about-btn">
+                  More Details
+                </Link>
+            </aside>
+          </div>
           <div className="slider">
             {data.map((image, imgIndex) => {
               const { img, id } = image;
@@ -69,6 +99,7 @@ const Home = () => {
               icon={faAngleRight}
             />
           </div>
+          <div className=""></div>
         </div>
       </div>
     </>
