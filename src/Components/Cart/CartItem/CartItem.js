bfed6fb066e625b233a5852a0e9c34038cd1f13f
Youssef_Mahmoud <100860879+Youssef-Mhmoud@users.noreply.github.com>
2022-08-18 07:22:51 +0200

    Design Cart Item

--- src/Components/Cart/CartItem/CartItem.js
+++ src/Components/Cart/CartItem/CartItem.js
@@ -9,7 +9,13 @@ const CartItem = ({product}) => {
         <div className="info-cart">
           <h4 className="title-cart">{product.title}</h4>
           <p className="price">{product.price}</p>
+          <div className="inc-dec">
+            <span className="inc">+</span>
+            <span className="number">0</span>
+            <span className="dec">-</span>
+          </div>
         </div>
+          <button className="remove-item">Remove</button>
       </div>
     </div>
   );
