7bf288f6348c8d25a5793201f8a656cbbbc78019
Youssef_Mahmoud <100860879+Youssef-Mhmoud@users.noreply.github.com>
2022-10-05 21:51:05 +0200

    Make animation more advanced

--- src/Components/Products/Product.js
+++ src/Components/Products/Product.js
@@ -9,30 +9,30 @@ import { faXmark } from "@fortawesome/free-solid-svg-icons";
 const Product = ({ product }) => {
   const dispatch = useDispatch();
 
-  const [zoomImg, setZoomImg] = useState(true);
+  const [zoomImg, setZoomImg] = useState();
 
   const showImg = () => {
-    if (zoomImg) {
+    // if (zoomImg) {
       return (
-        <div className="zoom-img">
+        <div className={`none ${zoomImg}`}>
           <div className="img-box">
             <img src={product.img} alt="img" />
             <FontAwesomeIcon
               icon={faXmark}
               className="x-mark"
-              onClick={() => setZoomImg(false)}
+              onClick={() => setZoomImg('animate2')}
             />
           </div>
         </div>
       );
-    }
+    // } 
   };
 
   return (
     <>
       {showImg()}
       <div className="watch-box">
-        <img src={product.img} onClick={() => setZoomImg(true)} alt="img" />
+        <img src={product.img} onClick={() => setZoomImg('zoom-img')} alt="img" />
         <div className="info-watch">
           <h4 className="title-watch">{product.title}</h4>
           <p className="price">${product.price}</p>
