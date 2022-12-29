650577669ab569356bdfaab001596864e3448c28
Youssef_Mahmoud <100860879+Youssef-Mhmoud@users.noreply.github.com>
2022-12-29 14:25:07 +0200

    more flexible to search about products

--- src/Data/ProductsData.js
+++ src/Data/ProductsData.js
@@ -1,6 +1,6 @@
 export default [
   {
-    sear: "airpod 1, ايربودز 1 ",
+    sear: "airpod 1, ايربودز 1 , airpods",
     id: 1,
     img: require("../assests/imgs/airpods/1-airpods.jpg"),
     title: "AirPods 1",
@@ -9,7 +9,7 @@ export default [
   },
   {
     id: 2,
-    sear: "airpod 2, ايربودز 2 ",
+    sear: "airpod 2, ايربودز 2 , airpods",
     img: require("../assests/imgs/airpods/2-airpods.jpg"),
     title: "AirPods 2",
     price: 770,
@@ -17,7 +17,7 @@ export default [
   },
   {
     id: 3,
-    sear: "airpod 3, ايربودز 3 ",
+    sear: "airpod 3, ايربودز 3 , airpods",
     img: require("../assests/imgs/airpods/3-airpods.jpg"),
     title: "AirPods 3",
     price: 250,
@@ -25,14 +25,14 @@ export default [
   },
   {
     id: 4,
-    sear: "airpod 4, ايربودز 4 ",
+    sear: "airpod 4, ايربودز 4 , airpods",
     img: require("../assests/imgs/airpods/4-airpods.jpg"),
     title: "AirPods 4",
     price: 1500,
     stars: 3,
   },
   {
-    sear: "headphone 1, سماعة 1 ",
+    sear: "headphone 1, سماعة 1 , headphones ",
     id: 5,
     img: require("../assests/imgs/headphone/1-headphone.jpg"),
     title: "HeadPhone 1",
@@ -40,7 +40,7 @@ export default [
     stars: 4,
   },
   {
-    sear: "headphone 2, سماعة 2 ",
+    sear: "headphone 2, سماعة 2 , headphones ",
     id: 6,
     img: require("../assests/imgs/headphone/2-headphone.jpg"),
     title: "HeadPhone 2",
@@ -48,7 +48,7 @@ export default [
     stars: 5,
   },
   {
-    sear: "headphone 3, سماعة 3 ",
+    sear: "headphone 3, سماعة 3 , headphones ",
     id: 7,
     img: require("../assests/imgs/headphone/3-headphone.jpg"),
     title: "HeadPhone 3",
@@ -56,7 +56,7 @@ export default [
     stars: 3,
   },
   {
-    sear: "headphone 4, سماعة 4 ",
+    sear: "headphone 4, سماعة 4 , headphones ",
     id: 8,
     img: require("../assests/imgs/headphone/4-headphone.jpg"),
     title: "HeadPhone 4",
@@ -64,7 +64,7 @@ export default [
     stars: 4,
   },
   {
-    sear: "headphone 5, سماعة 5 ",
+    sear: "headphone 5, سماعة 5 , headphones ",
     id: 9,
     img: require("../assests/imgs/headphone/5-headphone.jpg"),
     title: "HeadPhone 5",
@@ -72,7 +72,7 @@ export default [
     stars: 3,
   },
   {
-    sear: "headphone 6, سماعة 6 ",
+    sear: "headphone 6, سماعة 6 , headphones ",
     id: 10,
     img: require("../assests/imgs/headphone/6-headphone.jpg"),
     title: "HeadPhone 6",
@@ -80,7 +80,7 @@ export default [
     stars: 4,
   },
   {
-    sear: "watch 1, ساعة 1 ",
+    sear: "watch 1, ساعة 1 , watches",
     id: 11,
     img: require("../assests/imgs/watch/1-watch.jpg"),
     title: "Watch 1",
@@ -88,7 +88,7 @@ export default [
     stars: 5,
   },
   {
-    sear: "watch 2, ساعة 2 ",
+    sear: "watch 2, ساعة 2 , watches",
     id: 12,
     img: require("../assests/imgs/watch/2-watch.jpg"),
     title: "Watch 2",
@@ -96,7 +96,7 @@ export default [
     stars: 3,
   },
   {
-    sear: "watch 3, ساعة 3 ",
+    sear: "watch 3, ساعة 3 , watches",
     id: 13,
     img: require("../assests/imgs/watch/3-watch.jpg"),
     title: "Watch 3",
@@ -104,7 +104,7 @@ export default [
     stars: 5,
   },
   {
-    sear: "watch 4, ساعة 4 ",
+    sear: "watch 4, ساعة 4 , watches",
     id: 14,
     img: require("../assests/imgs/watch/4-watch.jpg"),
     title: "Watch 4",
@@ -112,7 +112,7 @@ export default [
     stars: 4,
   },
   {
-    sear: "watch 5, ساعة 5 ",
+    sear: "watch 5, ساعة 5 , watches",
     id: 15,
     img: require("../assests/imgs/watch/5-watch.jpg"),
     title: "Watch 5",
@@ -120,7 +120,7 @@ export default [
     stars: 2,
   },
   {
-    sear: "watch 6, ساعة 6 ",
+    sear: "watch 6, ساعة 6 , watches",
     id: 16,
     img: require("../assests/imgs/watch/6-watch.jpg"),
     title: "Watch 6",
@@ -128,7 +128,7 @@ export default [
     stars: 5,
   },
   {
-    sear: "watch 7, ساعة 7 ",
+    sear: "watch 7, ساعة 7 , watches",
     id: 17,
     img: require("../assests/imgs/watch/7-watch.jpg"),
     title: "Watch 7",
@@ -136,7 +136,7 @@ export default [
     stars: 3,
   },
   {
-    sear: "watch 8, ساعة 8 ",
+    sear: "watch 8, ساعة 8 , watches",
     id: 18,
     img: require("../assests/imgs/watch/8-watch.jpg"),
     title: "Watch 8",
